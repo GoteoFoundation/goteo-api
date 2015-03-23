e5854cadc429256064fbfd63c1bcc5d680d6015e
Ivan Vergés <ivan@microstudi.net>
2015-03-23 18:36:34 +0100

    add date filter for calls

--- api/models/call.py
+++ api/models/call.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 #from flask.ext.sqlalchemy import Pagination
-from sqlalchemy import func, Integer, String, Text, Date, DateTime, Float
+from sqlalchemy import func, Integer, String, Text, Date
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from api.helpers import image_url, utc_from_local
-from sqlalchemy import asc
-from api.decorators import cacher
-from api import db
+from sqlalchemy.orm.exc import MultipleResultsFound
+
+from ..decorators import cacher
+from .. import db
+from ..helpers import utc_from_local
 
 class Call(db.Model):
     __tablename__ = 'call'
@@ -26,20 +26,36 @@ class Call(db.Model):
     amount = db.Column('amount', Integer)
     owner = db.Column('owner', String(50))
     status = db.Column('status', Integer)
-    date_published = db.Column('published', Date)
+    opened = db.Column('opened', Date)
+    published = db.Column('published', Date)
 
     def __repr__(self):
         return '<Call %s: %s>' % (self.id, self.name)
 
+
+    @hybrid_property
+    def date_opened(self):
+        return utc_from_local(self.opened)
+
+    @hybrid_property
+    def date_published(self):
+        return utc_from_local(self.published)
+
     #Filters for this table
     @hybrid_property
     def filters(self):
-        return [self.status > self.STATUS_REVIEWING]
+        return [self.status > self.STATUS_EDITING]
 
     # Getting filters for this model
     @hybrid_method
     def get_filters(self, **kwargs):
         filters = self.filters
+        if 'from_date' in kwargs and kwargs['from_date'] is not None:
+            filters.append(self.opened >= kwargs['from_date'])
+        # Filters by "to date"
+        # counting license created before this date
+        if 'to_date' in kwargs and kwargs['to_date'] is not None:
+            filters.append(self.opened <= kwargs['to_date'])
 
         return filters
 
