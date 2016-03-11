be259d4563942b3d2dc7301308ce1e48f8147de9
Ivan Vergés <ivan@microstudi.net>
2016-03-11 21:51:46 +0100

    quick fix CORS

--- goteoapi/controllers.py
+++ goteoapi/controllers.py
@@ -8,10 +8,10 @@ from . import app
 def add_cors(resp):
     """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
         by the client. """
-    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
-    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers', 'Authorization')
+    # resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+    # resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    # resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    # resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers', 'Authorization')
     # set low for debugging
     if app.debug:
         resp.headers['Access-Control-Max-Age'] = '1'
