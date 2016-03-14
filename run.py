6d50b090ccc8ca4047ae952de52d2995f7d4a3d6
Ivan Vergés <ivan@microstudi.net>
2016-03-14 11:41:51 +0100

    API Modules by config

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from goteoapi import app
+import config
 
 # import sub-modules controllers
 # Minimal endpoints:
 __import__('goteoapi.controllers')
+
+# Additional modules from config
+if hasattr(config, 'MODULES'):
+    for i in config.MODULES:
+        __import__(i)
 
 #This part will not be executed under uWSGI module (nginx)
 if __name__ == '__main__':
