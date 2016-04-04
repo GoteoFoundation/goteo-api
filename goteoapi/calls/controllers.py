80dd8a9205b6b88a29532726dcc7093c7dad8e27
Ivan Vergés <ivan@microstudi.net>
2016-04-04 22:15:20 +0200

    matchfunding list

--- goteoapi/calls/controllers.py
+++ goteoapi/calls/controllers.py
@@ -8,9 +8,9 @@ from flask import url_for, redirect
 from .resources import CallsListAPI, CallAPI
 
 api.add_resource(CallsListAPI, '/matchfunding/', endpoint='api_calls.calls_list')
-api.add_resource(CallAPI, '/matchfunding/<string:call_id>', endpoint='api_calls.call')
+# api.add_resource(CallAPI, '/matchfunding/<string:call_id>', endpoint='api_calls.call')
 
 # redirect end trailing slash
-@app.route('/matchfunding/<string:call_id>/', endpoint='redirect.call')
-def call_redirect(call_id):
-    return redirect(url_for('api_calls.call', call_id=call_id), code=301)
+# @app.route('/matchfunding/<string:call_id>/', endpoint='redirect.call')
+# def call_redirect(call_id):
+#     return redirect(url_for('api_calls.call', call_id=call_id), code=301)
