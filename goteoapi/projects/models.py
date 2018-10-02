53e814f6229a1e85766fdf30bfd5cae8fea27b0f
Ivan Vergés <ivan@platoniq.net>
2018-10-02 13:53:42 +0200

    search projects by footprint

--- goteoapi/projects/models.py
+++ goteoapi/projects/models.py
@@ -289,6 +289,8 @@ class Project(db.Model):
         from ..location.models import ProjectLocation
         from ..calls.models import CallProject
         from ..matchers.models import MatcherProject
+        from ..sdgs.models import SdgSocialCommitment, SdgCategory
+        from ..footprints.models import FootprintSocialCommitment, FootprintCategory
 
         # Filters by default only published projects
         filters = []
@@ -391,6 +393,29 @@ class Project(db.Model):
 
         if 'social_commitment' in kwargs and kwargs['social_commitment'] is not None:
             filters.append(self.social_commitment_id.in_(as_list(kwargs['social_commitment'])))
+
+        if 'sdg' in kwargs and kwargs['sdg'] is not None:
+            sub1 = db.session.query(SdgSocialCommitment.social_commitment_id) \
+                .filter(SdgSocialCommitment.sdg_id.in_(as_list(kwargs['sdg'])))
+            and1 = and_(self.social_commitment_id != None,
+                self.social_commitment_id.in_(sub1))
+            sub2 = db.session.query(ProjectCategory.project_id) \
+                .filter(ProjectCategory.project_id == SdgCategory.category_id, SdgCategory.sdg_id.in_(as_list(kwargs['sdg'])))
+            and2 = and_(self.social_commitment_id == None,
+                self.id.in_(sub2))
+            filters.append(or_(and1, and2))
+
+        if 'footprint' in kwargs and kwargs['footprint'] is not None:
+            sub1 = db.session.query(FootprintSocialCommitment.social_commitment_id) \
+                .filter(FootprintSocialCommitment.footprint_id.in_(as_list(kwargs['footprint'])))
+            and1 = and_(self.social_commitment_id != None,
+                self.social_commitment_id.in_(sub1))
+            sub2 = db.session.query(ProjectCategory.project_id) \
+                .filter(ProjectCategory.project_id == FootprintCategory.category_id, FootprintCategory.footprint_id.in_(as_list(kwargs['footprint'])))
+            and2 = and_(self.social_commitment_id == None,
+                self.id.in_(sub2))
+            filters.append(or_(and1, and2))
+
 
         if 'loc_status' in kwargs and kwargs['loc_status'] is not None:
             if kwargs['loc_status'] == 'located':
